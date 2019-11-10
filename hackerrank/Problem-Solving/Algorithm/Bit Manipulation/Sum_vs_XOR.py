c0fe2227b68eee7b412b3069b5b8c3a8dd569b56
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2019-11-10 20:44:00 +0600

    Update Sum_vs_XOR.py

--- hackerrank/Problem-Solving/Algorithm/Bit Manipulation/Sum_vs_XOR.py
+++ hackerrank/Problem-Solving/Algorithm/Bit Manipulation/Sum_vs_XOR.py
@@ -1,12 +1,22 @@
 # Complete the sumXor function below.
+
+# It's not able pass all test case.
+#class Solution(object):
+#    def sumXor(n):
+#        count = 0
+#        for i in range(n):
+#            if n+i == n^i:
+#                count += 1
+#        return count
+
+
+
+# It's able to pass all test case.
 class Solution(object):
     def sumXor(n):
-        count = 0
-        for i in range(n):
-            if n+i == n^i:
-                count += 1
-        return count
-
+        num_of_zero = (bin(n).count('0')) - 1
+        return 2**num_of_zero
+    
 if __name__ == '__main__':
 
     n = int(input().strip())
