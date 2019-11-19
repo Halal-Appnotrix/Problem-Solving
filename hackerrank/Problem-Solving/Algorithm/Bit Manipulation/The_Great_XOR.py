043f2dd7be328d23e3fe5d713041207b773b5bdc
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2019-11-19 23:32:15 +0600

    Python3 Solution of The Great XOR.

---
+++ hackerrank/Problem-Solving/Algorithm/Bit Manipulation/The_Great_XOR.py
@@ -0,0 +1,14 @@
+def theGreatXor(x):
+    
+    return (1 << x.bit_length()) - x - 1
+
+if __name__ == '__main__':
+
+    q = int(input())
+
+    for q_itr in range(q):
+        x = int(input())
+
+        result = theGreatXor(x)
+
+        print(str(result) + '\n)
