47195341e5ac8edb63537502c9ed06676bcba37a
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2019-11-24 23:17:43 +0600

    Add files via upload

---
+++ hackerrank/Problem-Solving/Mysql/PADS.sql
@@ -0,0 +1,7 @@
+select concat(Name, case when occupation = "Doctor" Then "(D)"
+             when occupation = "Actor" Then "(A)"
+             when occupation = "Singer" then "(S)"
+             when occupation = "Professor" then "(P)"
+             end
+             )from Occupations order by Name, occupation;
+select "There are a total of", count(occupation), concat(lower(occupation), "s.") from Occupations group by occupation order by count(occupation) asc, occupation asc;
