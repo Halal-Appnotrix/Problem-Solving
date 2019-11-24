74d9ed0313d8b0dc69bb7fcb41d6ae4ece73c477
Halal Uddin <42236181+Halal375657@users.noreply.github.com>
2019-11-24 23:34:18 +0600

    Update PADS.sql

--- hackerrank/Problem-Solving/Mysql/PADS.sql
+++ hackerrank/Problem-Solving/Mysql/PADS.sql
@@ -6,4 +6,13 @@ select concat(Name, case when occupation = "Doctor" Then "(D)"
              when occupation = "Professor" then "(P)"
              end
              )from Occupations order by Name, occupation;
-select "There are a total of", count(occupation), concat(lower(occupation), "s.") from Occupations group by occupation order by count(occupation) asc, occupation asc;
+select "There are a total of", count(occupation), concat(lower(occupation), "s.")
+        from Occupations group by occupation order by count(occupation) asc, occupation asc;
+
+
+
+/* Another way */
+select concat(Name, '(', left(occupation, 1), ')') from Occupations order by name, occupation;
+
+select concat("There are a total of ", count(occupation), ' ', lower(occupation), 's.')
+        from Occupations group by occupation order by count(occupation), occupation;
